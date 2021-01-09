3b189c55f378cfbafc702a6b7861b98b821884ad
Bhavya337 <72799302+Bhavya337@users.noreply.github.com>
2021-01-09 12:04:38 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -47,7 +47,7 @@ name_with_tag="<h4>"+ name1 + "<img class='user_tick' src='tick.png' > </h4>" ;
 console.log(name_with_tag);
 message_with_tag="<h4 class='message_h4'>" + message + " </h4> ";
 console.log(message_with_tag);
-like_button="<button class='btn btn-warning' id=" + firebase_message_id + "value=" + like +"onclink='updatelike(this.id)' > " ;
+like_button="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updatelike(this.id)'>" ;
 span_with_tag="<span class='glyphicon glyphicon-thumbs-up' > like: "+  like + "</span> </button>  <hr> " ;
 row=name_with_tag + message_with_tag + like_button + span_with_tag ;
 document.getElementById("output").innerHTML+=row;
